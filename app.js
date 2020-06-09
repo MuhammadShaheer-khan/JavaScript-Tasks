604254b4ebe55e36cfaeb2865e96be52554cb5a8
MoaviaKhan <moaviakhan2001@gmail.com>
2020-06-09 12:21:23 +0500

    JavaScript Task1

---
+++ app.js
@@ -0,0 +1,16 @@
+// 1. Write a script to greet your website visitor using JS alert box.
+ alert("Welcome To Our Page.")
+
+// 2.
+ alert("Error! please enter a valid password.")
+
+// 3.
+ alert("Welcome to js land...\nHappy Coding!")
+
+// 4.
+ alert("Welcome to JS land...")
+
+ alert("Happy coding! \n This page is private")
+
+// 5.5. Generate the following message through browser’s developer console:
+// Done
